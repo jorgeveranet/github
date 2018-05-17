29c357b29928fe3a182b167e38bc3ab05de39696
Jorge Vera <me@jorgevera.net>
2018-05-17 15:01:24 -0400

    Add eslint and additional chai and mocha libs

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  globals: {
+    expect: true,
+    sinon: true
+  },
+  plugins: ['prettier', 'mocha'],
+  extends: [
+    'eslint:recommended',
+    'eslint-config-prettier'
+  ],
+  rules: {
+    'mocha/no-exclusive-tests': 'error',
+    'prettier/prettier': 'warn',
+    'no-console': 'off'
+  }
+}
