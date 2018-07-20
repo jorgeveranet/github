daca26752ff9e7d5d6cb1baefa920b372a11b585
Jorge Vera <me@jorgevera.net>
2018-07-19 23:38:13 -0400

    Implement repo lib
    A simple wrapper for octokit repos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-function greets() {
-  return 'Hi there...'
+require('dotenv').config()
+const Repo = require('./lib/repo')
+const uuidv1 = require('uuid/v1')
+
+const user = process.env.GITHUB_USER
+const password = process.env.GITHU_PASS
+
+const repo = new Repo(user, password)
+
+async function main() {
+  try {
+    const repoName = uuidv1()
+
+    const { clone_url: created } = await repo.create(repoName)
+    console.log('Repo created:', created)
+
+    const { clone_url: read } = await repo.get(repoName)
+    console.log('Repo read:', read)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-module.exports = { greets }
+main()
---
+++ src/lib/repo.js
@@ -0,0 +1,54 @@
+const octokit = require('@octokit/rest')({
+  timeout: 0,
+  headers: {
+    accept: 'application/vnd.github.v3+json',
+    'user-agent': 'octokit/rest.js v1.2.3'
+  },
+  baseUrl: 'https://api.github.com',
+  agent: undefined
+})
+
+let username
+let password
+
+class Repo {
+  constructor(user, pass) {
+    username = user
+    password = pass
+
+    octokit.authenticate({
+      type: 'basic',
+      username,
+      password
+    })
+  }
+
+  async get(name) {
+    try {
+      const { data } = await octokit.repos.get({
+        owner: username,
+        repo: name,
+        headers: {
+          accept: 'application/vnd.github.mercy-preview+json'
+        }
+      })
+      return data
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async create(name) {
+    try {
+      const { data } = await octokit.repos.create({
+        owner: username,
+        name
+      })
+      return data
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = Repo
