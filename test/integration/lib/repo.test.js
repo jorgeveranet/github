491f1001f5ba2071777ef4c812156fc5e0707bd8
Jorge Vera <me@jorgevera.net>
2018-07-20 22:21:20 -0400

    Start implementing integration test for repo

---
+++ test/integration/lib/repo.test.js
@@ -0,0 +1,32 @@
+const Repo = require('../../../src/lib/repo')
+const uuidv1 = require('uuid/v1')
+
+const user = process.env.GITHUB_USER
+const password = process.env.GITHU_PASS
+const repo = new Repo(user, password)
+
+describe('repo lib', () => {
+  it('creates, gets info, commit files and delete repo', async () => {
+    try {
+      const repoName = uuidv1()
+
+      // repo is created
+      const { clone_url: createdUrl } = await repo.create(repoName)
+      expect(createdUrl, 'repo is created').to.equal(
+        `https://github.com/${user}/${repoName}.git`
+      )
+
+      // gets info
+      const { clone_url: infoUrl } = await repo.get(repoName)
+      expect(infoUrl, 'gets info').to.be.equal(
+        `https://github.com/${user}/${repoName}.git`
+      )
+
+      // commit files
+
+      // delete repo
+    } catch (err) {
+      throw err
+    }
+  })
+})
--- test/unit/index.test.js
+++
@@ -1,7 +0,0 @@
-const { greets } = require('../../src/index')
-
-describe('index', () => {
-  it('greets', async () => {
-    expect(greets()).to.equal('Hi there...')
-  })
-})
